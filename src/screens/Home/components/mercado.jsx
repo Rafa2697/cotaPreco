d93479707b3170182f9e1bb186b742d182ec9a05
Rafael <Rafael.mattos468@gmail.com>
2024-05-29 18:46:52 -0300

    feature: consumindo a api da brasilapi.com.br

--- src/screens/Home/components/mercado.jsx
+++ src/screens/Home/components/mercado.jsx
@@ -1,37 +1,41 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList} from 'react-native'
 import React, { useState, useEffect } from 'react';
-import api from "../../../services/api";
+import Api from "../../../services/api";
 
 
 export default function Mercado() {
-  const [data, setData] = useState([]);
+  const [dados, setDados] = useState([]);
   useEffect(() => {
-    const fetchData = async () => {
-
+    async function fetchData() {
       try {
-        const response = await api.get('/clients');
-        setData(response.data);
+        const response = await Api.get(`/api/taxas/v1`);
+        
+        setDados(response.data);
+        console.log(dados)
       } catch (error) {
-        console.error('Erro ao buscar dados:', error);
+        console.error('Erro ao buscar dados dos clientes:', error);
       }
-      
     }
+
     fetchData();
   }, []);
-  const renderItem = ({ item }) => (
+  
+  const Item = ({nome, valor}) => (
     <View style={styles.item}>
-      <Text>{item.nome}</Text>
-      <Text>{item.email}</Text>
+      <Text style={styles.title}>{nome}</Text>
+      <Text style={styles.title}>{valor}</Text>
     </View>
   );
 
   return (
     <View style={styles.container}>
+      {/* <Text>{pessoas.nome}</Text>  */}
       <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
+        data={dados}
+        renderItem={({item}) => <Item nome={item.nome} valor={item.valor} />}
+        keyExtractor={item => item.nome}
       />
+     
     </View>
   )
 }
@@ -40,6 +44,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    
+  },
+  item: {
+    backgroundColor: '#425ae0',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+    color:'black'
+  },
 })
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const instance = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+ const api = axios.create({
+    baseURL: 'https://brasilapi.com.br'
   });
+
+  export default api;
