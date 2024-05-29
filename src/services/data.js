1e135737bc2f47b21e1b30935f11a1d029d4f1b9
Rafael <Rafael.mattos468@gmail.com>
2024-05-28 21:23:45 -0300

    update

---
+++ src/services/data.js
@@ -0,0 +1,49 @@
+
+export const data = [
+    {
+      "nome": "Maçã Fuji",
+      "valor": 5.99,
+      "local": {
+        "nomeEstabelecimento": "Supermercado ABC",
+        "cidade": "Peruíbe"
+      },
+      "data": "2024-05-27"
+    },
+    {
+      "nome": "Leite Integral",
+      "valor": 4.50,
+      "local": {
+        "nomeEstabelecimento": "Padaria do Sr. João",
+        "cidade": "Peruíbe"
+      },
+      "data": "2024-05-27"
+    },
+    {
+      "nome": "Carne Moída",
+      "valor": 19.99,
+      "local": {
+        "nomeEstabelecimento": "Açougue Central",
+        "cidade": "Peruíbe"
+      },
+      "data": "2024-05-27"
+    },
+    {
+      "nome": "Alface",
+      "valor": 2.50,
+      "local": {
+        "nomeEstabelecimento": "Feira Livre",
+        "cidade": "Peruíbe"
+      },
+      "data": "2024-05-27"
+    },
+    {
+      "nome": "Arroz Branco",
+      "valor": 5.30,
+      "local": {
+        "nomeEstabelecimento": "Empório Mundial",
+        "cidade": "Peruíbe"
+      },
+      "data": "2024-05-27"
+    }
+  ]
+  
