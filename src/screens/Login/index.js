95aeea3d689c926156198e621c6b10f99a3b8e84
Rafael <Rafael.mattos468@gmail.com>
2024-09-27 23:41:55 -0300

    fix: ajustando erros de ip

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -12,7 +12,7 @@ export default function Login({ navigation }) {
     }
 
     const handleLogin = async () => {
-        const apiUrl = 'http://192.168.1.5:3000/login'; //colocar o ip da maquina para usar o emulador ou aplicação no expo. 
+        const apiUrl = 'http://192.168.1.6:3000/login'; //colocar o ip da maquina para usar o emulador ou aplicação no expo. 
 
         const loginData = {
           ra: ra,
