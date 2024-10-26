896d2fbf5767f092c361b3c9b7c0e9c7b5247da6
Rafael <Rafael.mattos468@gmail.com>
2024-10-26 17:01:32 -0300

    Fix: ajuste no css

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -32,7 +32,7 @@ export default function Login({ navigation }) {
             if (response.ok) {
                 const jsonResponse = await response.json();
                 // Aqui você pode salvar o token de autenticação, se necessário, e navegar para a tela do aluno
-                console.log('Login bem-sucedido:', jsonResponse);
+                console.log( jsonResponse);
                 setLoading(false);
                 navigation.navigate('menuAluno'); // Navega para a tela do aluno após o login bem-sucedido
             } else {
@@ -70,7 +70,8 @@ export default function Login({ navigation }) {
 
                 <Text style={{ textAlign: 'right', color: 'blue' }} onPress={() => navigation.navigate('Cadastro')} >Primeiro acesso ?</Text>
             </View>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+            <View style={{  justifyContent: 'center', alignItems: 'center' }}>
                 {loading ? (
                     <ActivityIndicator size="large" color="#0000ff" />
                 ) : (
